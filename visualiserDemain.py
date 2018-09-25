cb12d41f67b69a9b023e241714f8912bc7b9aa1a
slamSIO <slam@siolla.fr>
2018-09-25 10:16:55 +0200

    Initial

---
+++ visualiserDemain.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+from datetime import date
+from datetime import timedelta
+
+
+def getDemain() :
+	
+	return None
+
+
+if __name__ == '__main__' :
+	
+	jourDemain = getDemain()
+	
+	print( 'Demain nous serons le' , jourDemain )
+
+
